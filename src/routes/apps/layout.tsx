e3bddf064704b0740dcff660d80b0c28053601f2
BN-20220507YIDJ\BFChainer <494057020@qq.com>
2023-09-19 15:14:14 +0800

    删除多余代码

--- src/routes/apps/layout.tsx
+++ src/routes/apps/layout.tsx
@@ -5,10 +5,43 @@ import { component$, Slot, useStyles$, createContextId, useContextProvider, useS
 import Header from "~/components/dashboard/header/header";
 import Footer from "~/components/dashboard/footer/footer";
 
-import AppModel from '~/model/app'; // 从model中导入App
+// import AppModel from '~/model/app'; // 从model中导入App
+
+const apps = [
+  {
+    "name": "fwefwe",
+    "url": "https://www.travelpalapp.com",
+    "image": "app_image_url_1",
+    "description": "f11332"
+  },
+  {
+    "name": "d'w'd'w",
+    "url": "https://www.travelpalapp.com",
+    "image": "app_image_url_1",
+    "description": "fefefefe"
+  },
+  {
+    "name": "f2qfe2f",
+    "url": "https://www.travelpalapp.com",
+    "image": "app_image_url_1",
+    "description": "1111"
+  },
+  {
+    "name": "fefe",
+    "url": "https://www.travelpalapp.com",
+    "image": "app_image_url_1",
+    "description": "fwefwef"
+  },
+  {
+    "name": "rocking",
+    "url": "https://www.travelpalapp.com",
+    "image": "app_image_url_1",
+    "description": "rockingr313r1"
+  }
+]
 
 export const useAppList = routeLoader$(async (requestEvent) => {
-    const apps = AppModel.all()
+    // const apps = AppModel.all()
     // console.log('loadAll', apps)
     return apps
 });
@@ -17,6 +50,7 @@ export const useAppList = routeLoader$(async (requestEvent) => {
 interface searchBarData {
     text: string;
 }
+
 // 这里放 context，因为在index 里放context，会导致报错
 export const seachBarContext = createContextId<searchBarData>('apps.seachBarText');
 
